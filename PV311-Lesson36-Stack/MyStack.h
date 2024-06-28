b4b5504732e3d4d6ec864fca9b6386c01e65e991
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-28 19:46:56 +0300

    void push(U value)

--- PV311-Lesson36-Stack/MyStack.h
+++ PV311-Lesson36-Stack/MyStack.h
@@ -4,18 +4,46 @@ using std::cout;
 using std::endl;
 
 namespace MyStack {
-	template <typename T>
+
+	template <typename Type>
 	class DNode {
 	public:
-		T info;
+		Type info;
 		DNode* prev;
 		DNode* next;
 
-		DNode(T value) {
+		DNode(Type value) {
 			info = value;
 			prev = next = nullptr;
 		}
 	};
 
+	template <typename U>
+	class Stack {
+		//LIFO = last in first out
+		DNode<U>* head; //голова = адреса першого вузла
+		DNode<U>* tail; //хвіст = адреса останнього вузла
+	public:
+		Stack() {
+			head = tail = nullptr;
+		}
+		~Stack() { }
 
+		bool isEmpty()const {
+			return head == nullptr || tail == nullptr;
+		}
+
+		void push(U value) {
+			DNode<U>* el = new DNode<U>(value);
+
+			if (isEmpty()) head = tail = el;
+			else {
+				el->prev = tail;
+				tail->next = el;
+				tail = el;
+			}
+		}
+
+
+	};
 }
